a480951745a1d68004f8f5b9a36beb7878073b5e
Bat004 <b23173246@gmail.com>
2026-03-09 09:46:28 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module school.coda.baptiste_josserand.bataille_javale {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens school.coda.baptiste_josserand.bataille_javale to javafx.fxml;
+    exports school.coda.baptiste_josserand.bataille_javale;
+}
---
+++ src/main/java/school/coda/baptiste_josserand/bataille_javale/Launcher.java
@@ -0,0 +1,9 @@
+package school.coda.baptiste_josserand.bataille_javale;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
