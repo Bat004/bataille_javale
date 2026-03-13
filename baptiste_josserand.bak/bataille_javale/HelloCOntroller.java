bb7322863b135102ec95ee5dd960cf3bc1bb261c
Josserand Sellier <josse.sellier@gmail.com>
2026-03-13 10:50:07 +0100

    lancement

---
+++ baptiste_josserand.bak/bataille_javale/HelloCOntroller.java
@@ -0,0 +1,14 @@
+package school.coda.baptiste_josserand.bataille_javale;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
