c54c3079b9fa590d15736b1e5dc2f6b598f573e7
Marc Bouvier <m.bouvier.dev@gmail.com>
2026-03-15 19:38:34 +0100

    correction: Lancement - adaptation Maven et modules java pour charger les sons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module school.coda.baptiste{
+module bataille.javale {
     requires javafx.controls;
-    requires javafx.fxml;
+    requires javafx.graphics;
     requires javafx.media;
 
-
-    opens school.coda.baptiste to javafx.fxml;
+    opens school.coda.baptiste to javafx.fxml, javafx.controls, javafx.media;
+    opens images;
+    opens sounds;
     exports school.coda.baptiste;
 }
